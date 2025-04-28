e482b0ec04d98a7980f3287ccf06c1efb382a005
Victor <psygreg@pm.me>
2025-04-28 02:43:21 -0300

    cachy fix

--- patcher.sh
+++ patcher.sh
@@ -39,12 +39,12 @@ GPU=$(lspci | grep -i '.* vga .* nvidia .*')
 shopt -s nocasematch
 if [ "$SHELL" = "$(which fish)" ] || [ "$FISH_VERSION" ]; then
     if [[ $GPU == *' nvidia '* ]]; then
-        set -x --universal __GL_SHADER_DISK_CACHE_SIZE 12000000000
+        set -x --universal __GL_SHADER_DISK_CACHE_SIZE '12000000000'
         echo "$SUCCESS"
         sleep 5
         exit 0
     else
-        set -x --universal MESA_SHADER_CACHE_MAX_SIZE 12G
+        set -x --universal MESA_SHADER_CACHE_MAX_SIZE '12G'
         echo "$SUCCESS"
         sleep 5
         exit 0
