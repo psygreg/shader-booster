c964f5198a781733e93c99f0c9908d9a84a487c5
Victor <psygreg@pm.me>
2025-05-17 14:06:11 -0300

    use whiptail for user messages, clean up after patch

--- patcher.sh
+++ patcher.sh
@@ -1,71 +1,89 @@
 #!/bin/bash
-# vars
-
-SUCCESS="Success! Reboot to apply."
-APPLIED="Patch already present in '$DEST_FILE'. Nothing to do."
 
 # functions
 
 patch_nv () {
     PATCH_CONTENT=$(<"patch-nvidia")
     if grep -Fxq "$PATCH_CONTENT" "$DEST_FILE"; then
-        echo "$APPLIED"
-        sleep 5
+        whiptail --title "Shader Booster" --msgbox "System already patched. Nothing to do" 8 78
+        cd ..
+        rm -rf spatcher
         exit 0
     else
         cat "patch-nvidia" >> "$DEST_FILE"
-        echo "$SUCCESS"
-        sleep 5
+        whiptail --title "Shader Booster" --msgbox "Success! Reboot to apply." 8 78
+        cd ..
+        rm -rf spatcher
         exit 0
     fi
 }
 patch_mesa () {
     PATCH_CONTENT=$(<"patch-mesa")
     if grep -Fxq "$PATCH_CONTENT" "$DEST_FILE"; then
-        echo "$APPLIED"
-        sleep 5
+        whiptail --title "Shader Booster" --msgbox "System already patched. Nothing to do" 8 78
+        cd ..
+        rm -rf spatcher
         exit 0
     else
         cat "patch-mesa" >> "$DEST_FILE"
-        echo "$SUCCESS"
-        sleep 5
+        whiptail --title "Shader Booster" --msgbox "Success! Reboot to apply." 8 78
+        cd ..
+        rm -rf spatcher
         exit 0
     fi
 }
 
 # runtime
 
+sudo apt install wget whiptail
 GPU=$(lspci | grep -i '.* vga .* nvidia .*')
 shopt -s nocasematch
 if [[ -f "$HOME/.bash_profile" ]]; then
     DEST_FILE="$HOME/.bash_profile"
     if [[ $GPU == *' nvidia '* ]]; then
+        cd $HOME
+        mkdir spatcher
+        cd spatcher
         wget -O patch-nvidia https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-nvidia;
         patch_nv
     else
+        cd $HOME
+        mkdir spatcher
+        cd spatcher
         wget -O patch-mesa https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-mesa;
         patch_mesa
     fi
 elif [[ -f "$HOME/.profile" ]]; then
     DEST_FILE="$HOME/.profile"
     if [[ $GPU == *' nvidia '* ]]; then
+        cd $HOME
+        mkdir spatcher
+        cd spatcher
         wget -O patch-nvidia https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-nvidia;
         patch_nv
     else
+        cd $HOME
+        mkdir spatcher
+        cd spatcher
         wget -O patch-mesa https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-mesa;
         patch_mesa
     fi
 elif [[ -f "$HOME/.zshrc" ]]; then
     DEST_FILE="$HOME/.zshrc"
     if [[ $GPU == *' nvidia '* ]]; then
+        cd $HOME
+        mkdir spatcher
+        cd spatcher
         wget -O patch-nvidia https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-nvidia;
         patch_nv
     else
+        cd $HOME
+        mkdir spatcher
+        cd spatcher
         wget -O patch-mesa https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-mesa;
         patch_mesa
     fi
 else
-    echo "No valid shell found, aborting..."
-    sleep 5
+    whiptail --title "Shader Booster" --msgbox "No valid shell found." 8 78
     exit 1
 fi
