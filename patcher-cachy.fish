d53fed8de7e68b42f9545c2e6a6320dfca11214d
Victor <psygreg@pm.me>
2025-05-19 16:53:41 -0300

    cleanup, better dependency check, store system patched state

--- patcher-cachy.fish
+++ patcher-cachy.fish
@@ -1,7 +1,4 @@
 #!/bin/fish
-# vars
-SUCCESS="Success! Reboot to apply."
-APPLIED="Patch already present in '$DEST_FILE'. Nothing to do."
 
 # runtime
 GPU=$(lspci | grep -i '.* vga .* nvidia .*')
@@ -9,14 +6,10 @@ shopt -s nocasematch
 if command -v fish &>/dev/null; then
     if [[ $GPU == *' nvidia '* ]]; then
         set -x --universal __GL_SHADER_DISK_CACHE_SIZE '12000000000'
-        echo "$SUCCESS"
-        sleep 5
         exit 0
     else
         set -x --universal AMD_VULKAN_ICD 'RADV'
         set -x --universal MESA_SHADER_CACHE_MAX_SIZE '12G'
-        echo "$SUCCESS"
-        sleep 5
         exit 0
     fi
 else
--- patcher.sh
+++ patcher.sh
@@ -1,62 +1,76 @@
 #!/bin/bash
-
 # functions
 
+# dependency checker
+depcheck () {
+
+    local dependencies=(wget whiptail)
+    for dep in "${dependencies[@]}"; do
+        if dpkg -s "$dep" 2>/dev/null 1>&2; then
+            continue
+        else
+            sudo apt install -y "$dep"
+        fi
+    done
+
+}
+
+# patch for Nvidia GPUs
 patch_nv () {
 
+    cd $HOME
+    wget -O patch-nvidia https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-nvidia;
     cat "${HOME}/patch-nvidia" >> "${DEST_FILE}"
     whiptail --title "Shader Booster" --msgbox "Success! Reboot to apply." 8 78
+    cat "1" > "${HOME}/.booster"
     rm ${HOME}/patch-nvidia
     exit 0
 
 }
+
+# patch for Mesa-driven GPUs
 patch_mesa () {
 
+    cd $HOME
+    wget -O patch-mesa https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-mesa;
     cat "${HOME}/patch-mesa" >> "${DEST_FILE}"
     whiptail --title "Shader Booster" --msgbox "Success! Reboot to apply." 8 78
+    cat "1" > "${HOME}/.booster"
     rm ${HOME}/patch-mesa
     exit 0
 
 }
 
 # runtime
-
-sudo apt install wget whiptail
+depcheck
 GPU=$(lspci | grep -i '.* vga .* nvidia .*')
-if [[ -f "${HOME}/.bash_profile" ]]; then
-    DEST_FILE="${HOME}/.bash_profile"
-    if [[ $GPU == *' nvidia '* ]]; then
-        cd $HOME
-        wget -O patch-nvidia https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-nvidia;
-        patch_nv
+if [ ! -f ${HOME}/.booster ]; then
+    if [[ -f "${HOME}/.bash_profile" ]]; then
+        DEST_FILE="${HOME}/.bash_profile"
+        if [[ $GPU == *' nvidia '* ]]; then
+            patch_nv
+        else
+            patch_mesa
+        fi
+    elif [[ -f "$HOME/.profile" ]]; then
+        DEST_FILE="${HOME}/.profile"
+        if [[ $GPU == *' nvidia '* ]]; then
+            patch_nv
+        else
+            patch_mesa
+        fi
+    elif [[ -f "$HOME/.zshrc" ]]; then
+        DEST_FILE="${HOME}/.zshrc"
+        if [[ $GPU == *' nvidia '* ]]; then
+            patch_nv
+        else
+            patch_mesa
+        fi
     else
-        cd $HOME
-        wget -O patch-mesa https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-mesa;
-        patch_mesa
-    fi
-elif [[ -f "$HOME/.profile" ]]; then
-    DEST_FILE="${HOME}/.profile"
-    if [[ $GPU == *' nvidia '* ]]; then
-        cd $HOME
-        wget -O patch-nvidia https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-nvidia;
-        patch_nv
-    else
-        cd $HOME
-        wget -O patch-mesa https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-mesa;
-        patch_mesa
-    fi
-elif [[ -f "$HOME/.zshrc" ]]; then
-    DEST_FILE="${HOME}/.zshrc"
-    if [[ $GPU == *' nvidia '* ]]; then
-        cd $HOME
-        wget -O patch-nvidia https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-nvidia;
-        patch_nv
-    else
-        cd $HOME
-        wget -O patch-mesa https://raw.githubusercontent.com/psygreg/shader-booster/refs/heads/main/patch-mesa;
-        patch_mesa
+        whiptail --title "Shader Booster" --msgbox "No valid shell found." 8 78
+        exit 1
     fi
 else
-    whiptail --title "Shader Booster" --msgbox "No valid shell found." 8 78
-    exit 1
+    whiptail --title "Shader Booster" --msgbox "System already patched." 8 78
+    exit 0
 fi
