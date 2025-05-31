3e1ba8b4431d1a4122488f56e2086104e3d43a3c
Victor <psygreg@pm.me>
2025-05-31 01:48:01 -0300

    fix dependency checking

--- patcher.sh
+++ patcher.sh
@@ -5,15 +5,15 @@
 depcheck () {
 
     local dependencies=()
-    if [[ "$ID_LIKE" =~ (suse|rhel|fedora) ]]; then
+    if [[ "$ID_LIKE" =~ (suse|rhel|fedora) ]] || [[ "$ID" =~ (fedora|suse) ]]; then
         dependencies=(wget newt)
-    elif [ "$ID" == "arch" ]; then
+    elif [ "$ID" == "arch" ] || [[ "$ID_LIKE" =~ (arch) ]]; then
         dependencies=(wget libnewt)
-    elif [[ "$ID_LIKE" =~ (ubuntu|debian) ]]; then
+    elif [[ "$ID_LIKE" =~ (ubuntu|debian) ]] || [ "$ID" == "debian" ]; then
         dependencies=(wget whiptail)
     fi
     for dep in "${dependencies[@]}"; do
-        if [[ "$ID_LIKE" =~ (suse|rhel|fedora) ]]; then
+        if [[ "$ID_LIKE" =~ (suse|rhel|fedora) ]] || [[ "$ID" =~ (fedora|suse) ]]; then
             if rpm -qi "$dep" 2>/dev/null 1>&2; then
                 continue
             else
@@ -23,13 +23,13 @@ depcheck () {
                     sudo dnf in "$dep" -y
                 fi
             fi
-        elif [ "$ID" == "arch" ]; then
+        elif [ "$ID" == "arch" ] || [[ "$ID_LIKE" =~ (arch) ]]; then
             if pacman -Qi "$dep" 2>/dev/null 1>&2; then
                 continue
             else
                 sudo pacman -S --noconfirm "$dep"
             fi
-        elif [[ "$ID_LIKE" =~ (ubuntu|debian) ]]; then
+        elif [[ "$ID_LIKE" =~ (ubuntu|debian) ]] || [ "$ID" == "debian" ]; then
             if dpkg -s "$dep" 2>/dev/null 1>&2; then
                 continue
             else
